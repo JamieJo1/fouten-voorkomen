66d20921b1b4696ea4346eceb9317e5d4bcccb1c
JamieJo1 <43269701+JamieJo1@users.noreply.github.com>
2018-10-05 13:13:54 +0200

    Update bestand-definitief-versie2-DEF.php

--- bestand-definitief-versie2-DEF.php
+++ bestand-definitief-versie2-DEF.php
@@ -1,18 +1,31 @@
 <?php
+
 defined('BASEPATH') or die;
-function SetNameCorrect($string) {
-$hString = (ucfirst($string));
-return $hString;
+
+function SetNameCorrect($string) 
+{
+  $hString = (ucfirst($string));
+  return $hString;
 }
-function realPrice ($getal) {
-if ( $getal == null ) {
-return null;
-} else {
-$real = "&euro; ";
-$real .= number_format($getal, 2, ',', '.');
-return $real;
-}}
+
+function realPrice ($getal) 
+{
+  if ( $getal == null ) 
+  {
+    return null;
+  } 
+ else
+ {
+    $real = "&euro; ";
+    $real .= number_format($getal, 2, ',', '.');
+    return $real;
+ } 
+}
+
 $cheeses = array('gouda', 'old', 'parasan');
+
 array_push($cheeses, 'mozerella');
+
 sort($cheeses);
+
 print_r($cheeses);
